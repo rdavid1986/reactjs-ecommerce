dccf6aed0169aed4587fba94b83d1bb23e113453
rdavid1986 <rdavid1986@outlook.com>
2022-12-02 16:42:29 -0300

    para merge develop

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,16 +7,22 @@ function NavBar() {
     <nav className="container__nav">
       <ul className="nav__ul">
         <li className="nav__li">
-          <Link to="/category/smartphones">SmartPhones</Link>
-        </li>
         <li className="nav__li">
-          <Link to="/category/AMD-Radeon">AMD-Radeon</Link>
+          <Link to="/category/tv">Smart tv</Link>
+        </li>
+          <Link to="/category/smartphones">SmartPhones</Link>
         </li>
         <li className="nav__li">
           <Link to="/category/laptops">LapTops</Link>
         </li>
         <li className="nav__li">
+          <Link to="/category/videogames">Video Games</Link>
+        </li>
+        <li className="nav__li">
           <Link to="/category/Nvidia">Nvidia</Link>
+        </li>
+        <li className="nav__li">
+          <Link to="/category/AMD-Radeon">AMD-Radeon</Link>
         </li>
         <li className="nav__li">
           <Link to="#">
--- src/data/firebase.js
+++ src/data/firebase.js
@@ -14,5 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export default function testDataBase() {
-    console.log(app);
+    console.log(app, "prueba de firabase");
 }
