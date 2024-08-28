5af91671cc6a58102674f3653d870bda600d817f
rdavid1986 <rdavid1986@outlook.com>
2024-08-28 18:40:10 -0300

    product detail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -17,14 +17,14 @@ function ItemDetail({product}) {
     return (
         <div className="card__detail">
             <div className="card__detail__img">
-                <img src={product.imgurl} alt="Product img" />
+                <img className="detail__img"src={product.imgurl} alt="Product img" />
             </div>
             <div className="card__detail__description">
                 <h2>{product.title}</h2>
                 <p>{product.description}</p>
                 <h3 className="card__detail__price">$ {product.price}</h3>
-            </div>
                 <ItemCount  onAddToCart={onAddToCart} stock={product.stock}  />
+            </div>
         </div>
     )
 }
